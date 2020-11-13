755df751f59e199e372092af94661622a6ed0c58
mikhail <mikle@shomov.spb.ru>
2020-11-14 01:13:04 +0300

    fix

--- src/lesson5/OpenAddressingSet.java
+++ src/lesson5/OpenAddressingSet.java
@@ -163,9 +163,9 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
         @Override
         public void remove() {
             if (index == 0) throw new IllegalStateException();
-            storage[index] = removed;
+            storage[index - 1] = removed;
+            count--;
             size--;
-            index--;
         }
     }
 }
